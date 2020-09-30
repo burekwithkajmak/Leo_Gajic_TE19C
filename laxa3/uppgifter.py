43cb7b336e8ddfb1952ab2bf5fec486ff9881865
1burek1 <39457472+1burek1@users.noreply.github.com>
2020-09-30 13:37:55 +0200

    Läxa3

---
+++ laxa3/uppgifter.py
@@ -0,0 +1,21 @@
+#1
+#a)
+for i in range(1,11):
+    print(i)
+print("Nästa uppgift")
+#b)
+for i in range(-10,11):
+    print(i)
+print("Nästa uppgift")
+#c)
+for i in range(10,-1,-1):
+    print(i)
+
+#3
+print("Nästa uppgift")
+s = 0
+for i in range(101):
+    s += i
+print(f"1+2+3.....+99+100 = {s}")
+
+#4
