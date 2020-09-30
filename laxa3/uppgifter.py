aea5c3e1e4a27cc145a8a916cce9a7598d6c0471
burekwithkajmak <70268893+burekwithkajmak@users.noreply.github.com>
2020-09-30 17:10:37 +0200

    Update uppgifter.py

--- laxa3/uppgifter.py
+++ laxa3/uppgifter.py
@@ -1,21 +1,28 @@
-#1
-#a)
+
+print("Uppgift 1a")
 for i in range(1,11):
     print(i)
-print("Nästa uppgift")
-#b)
+
+print("Uppgift 1b")
 for i in range(-10,11):
     print(i)
-print("Nästa uppgift")
-#c)
+print("Uppgift 1c")
+
 for i in range(10,-1,-1):
     print(i)
+print("Uppgift 2")
 
-#3
-print("Nästa uppgift")
+for x in range(1,101):
+    total = 0
+    for g in range(2,(x//3)):
+        if x%g==0:
+            total = total +1
+            break
+    if total== 0 and x !=1:
+        print(x)
+
+print("Uppgift 3")
 s = 0
 for i in range(101):
     s += i
 print(f"1+2+3.....+99+100 = {s}")
-
-#4
