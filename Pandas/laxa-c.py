560bed4c462c1a88c15efd032702e9f300a70426
burekwithkajmak <70268893+burekwithkajmak@users.noreply.github.com>
2021-04-16 08:46:23 +0200

    Cleanup &update

--- Pandas/laxa-c.py
+++ Pandas/laxa-c.py
@@ -23,14 +23,7 @@ print(summa2)
 
 labels = 'Förvärvsarbetade', 'Icke förvärvsarbetade'
 storlek = [summa1,summa2]
-explode = (0, 0.1)
 
-fig1, ax1 = plt.subplots()
-ax1.pie(storlek, explode=explode, labels=labels, autopct='%1.1f%%',
+plt.pie(storlek, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
-ax1.axis('equal') 
-
 plt.show()
-
-
-
